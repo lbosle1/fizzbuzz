2d6401cacfb3db1f2470cd2b953b2943bb0e1914
lbosle1 <31141165+lbosle1@users.noreply.github.com>
2017-09-26 15:00:37 -0400

    Add files via upload

--- app.rb
+++ app.rb
@@ -1,19 +1,35 @@
 # numbers = []
-counter = 1
 
+
+
+def user_unput()
+	puts "What's your favorite color?"
+	color = gets.chomp
+counter = 1
+	puts "What's your favorite number?"
+	number = gets.chomp
+counter = 1
+	puts "What's your favorite food"
+	food = gets.chomp
+counter = 1
 100.times do
 	puts counter
 	# numbers.push(counter)
 	counter = counter + 1
 	if counter % 15 == 0
-		print "fizzbuzz"
+		puts color
 	elsif counter % 3 == 0
-		print "fizz"
-	elsif counter % 5 == 0
-		print "buzz"
+		puts number
+		elsif counter % 5 == 0
+		puts food
 	else
 		puts counter
 	end
+end
+end
+
+1.times do
+	user_unput()
 end
 
 # puts numbers
